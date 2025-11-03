e4c9753536591d54a89cc5e8b1c3164eb98095ad
sunghun <nokdonwithkj211@gmail.com>
2025-11-03 18:15:11 +0900

    Feat(Bonus): add validate on new class, Bonus

--- src/main/java/lotto/domain/Bonus.java
+++ src/main/java/lotto/domain/Bonus.java
@@ -1,4 +1,25 @@
 package lotto.domain;
 
+import lotto.exception.ExceptionMessage;
+
 public class Bonus {
+    private final int bonusNumber;
+
+    public Bonus(int bonusNumber, Lotto lotto) {
+        validateRange(bonusNumber);
+        validateSameNumberInLotto(lotto, bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateRange(int bonusNumber) {
+        if (bonusNumber > Lotto.MAX_VALUE || bonusNumber < Lotto.MIN_VALUE) {
+            throw new IllegalArgumentException(ExceptionMessage.OUT_OF_RANGE_BONUS_NUMBER.getErrorDescription());
+        }
+    }
+
+    private void validateSameNumberInLotto(Lotto lotto, int bonusNumber) {
+        if (lotto.hasSameNumber(bonusNumber)) {
+            throw new IllegalArgumentException(ExceptionMessage.SAME_NUMBER_WITH_LOTTO_NUMBER.getErrorDescription());
+        }
+    }
 }
