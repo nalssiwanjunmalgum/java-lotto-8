18468c5fd6ca3fba62dcdf12085be719a56233c1
sunghun <nokdonwithkj211@gmail.com>
2025-11-03 17:30:41 +0900

    Feat: create exception enum classes for presenting problems and net moves

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -1,4 +1,25 @@
 package lotto.exception;
 
-public class ExceptionMessage {
+public enum ExceptionMessage {
+    WRONG_LOTTO_SIZE("6개의 숫자를 입력하지 않았습니다.", ModifyingType.LOTTO_NUMBER),
+    DUPLICATED_NUMBER("중복된 숫자를 입력했습니다.", ModifyingType.LOTTO_NUMBER),
+    OUT_OF_RANGE_LOTTO_NUMBER("범위에서 벗어난 수를 입력했습니다. 로또 번호 범위는 1~45까지입니다",
+            ModifyingType.LOTTO_NUMBER),
+    ;
+
+    private static final String PREFIX = "[ERROR] ";
+    private final String errorDescription;
+    private final ModifyingType modifyingType;
+
+    ExceptionMessage(String errorDescription, ModifyingType modifyingType) {
+        this.errorDescription = errorDescription;
+        this.modifyingType = modifyingType;
+    }
+
+    public String getErrorDescription() {
+        return PREFIX
+                + errorDescription
+                + modifyingType.getModifyingMessage();
+    }
 }
+
--- src/main/java/lotto/exception/ModifyingType.java
+++ src/main/java/lotto/exception/ModifyingType.java
@@ -1,4 +1,17 @@
 package lotto.exception;
 
-public class ModifyingType {
+public enum ModifyingType {
+    LOTTO_NUMBER(" 로또 번호을 다시 입력해주세요."),
+
+    ;
+
+    ModifyingType(String errorType) {
+        this.modifyingTypeMessage = errorType;
+    }
+
+    private final String modifyingTypeMessage;
+
+    public String getModifyingMessage() {
+        return modifyingTypeMessage;
+    }
 }
