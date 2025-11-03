f5d6b7e0996276d405c4346b80e71844ef2fd5e0
sunghun <nokdonwithkj211@gmail.com>
2025-11-03 19:39:28 +0900

    Feat(UserMoneyTest): add test code on UserMoney

---
+++ src/test/java/lotto/domain/UserMoneyTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class UserMoneyTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "11", "1111"})
+    void 정해진_단위로_떨어지지_않는다면_예외(String inputMoney) {
+        assertThatThrownBy(() -> new UserMoney(Parser.inputToNumber(inputMoney)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("단위의");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1000", "0", "200000"})
+    void 정해진_단위로_떨어진다면_성공(String inputMoney) {
+        assertThatNoException().isThrownBy(
+                () -> new UserMoney(Parser.inputToNumber(inputMoney)));
+    }
+}
