baedf3902be7f815fe92cf4b7eba60446b2a246b
sunghun <nokdonwithkj211@gmail.com>
2025-11-03 17:36:03 +0900

    Test(LottoTest): add test codes on Lotto's constructor

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,5 +23,42 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Nested
+    @DisplayName("예외가 발생하는 조건")
+    class LottoExcpetionCases {
+        @DisplayName("로또 번호의 개수가 6개보다 적다면 예외가 발생한다.")
+        @Test
+        void createLottoByLowerSize() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+        @Test
+        void createLottoByOverSize() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+        @Test
+        void createLottoByDuplicatedNumber() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("로또 번호에 범위에서 벗어난 숫자가 있으면 예외가 발생한다")
+        @Test
+        void createLottoByOutOfRangeNumber() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("로또 번호가 서로 다른 6개의 숫자인 경우 예외가 발생하지 않는다")
+        @Test
+        void createLottoByDifferentSixNumbers() {
+            assertThatCode(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)))
+                    .doesNotThrowAnyException();
+        }
+    }
 }
