0ed43debc84bac240911fb09b55efbc20bec5f32
sunghun <nokdonwithkj211@gmail.com>
2025-11-03 18:15:42 +0900

    Feat(LottoWithBonus): create Lotto with bonus number

--- src/main/java/lotto/domain/LottoWithBonus.java
+++ src/main/java/lotto/domain/LottoWithBonus.java
@@ -1,4 +1,11 @@
 package lotto.domain;
 
 public class LottoWithBonus {
+    private final Lotto userLotto;
+    private final Bonus userBonus;
+
+    public LottoWithBonus(Lotto lotto, Bonus bonus) {
+        this.userLotto = lotto;
+        this.userBonus = bonus;
+    }
 }
