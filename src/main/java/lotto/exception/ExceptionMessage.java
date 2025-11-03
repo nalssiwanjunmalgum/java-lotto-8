9bcdb9705d8ce390fe89a1ad297fd1ef86347e55
sunghun <nokdonwithkj211@gmail.com>
2025-11-03 18:00:31 +0900

    Feat(NumberValidator): create validator class for validating number pattern

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -5,6 +5,14 @@ public enum ExceptionMessage {
     DUPLICATED_NUMBER("중복된 숫자를 입력했습니다.", ModifyingType.LOTTO_NUMBER),
     OUT_OF_RANGE_LOTTO_NUMBER("범위에서 벗어난 수를 입력했습니다. 로또 번호 범위는 1~45까지입니다",
             ModifyingType.LOTTO_NUMBER),
+    NULL_INPUT("아무것도 입력하지 않았습니다.", ModifyingType.EVERY_NUMBER),
+    NOT_MATCHES_NUMBER_PATTERN("숫자를 입력하지 않았습니다.", ModifyingType.EVERY_NUMBER),
+    MULTIPLE_LETTERS_STARTS_WITH_ZERO("0으로 시작하는 수는 0만 가능합니다.", ModifyingType.EVERY_NUMBER),
+    SAME_NUMBER_WITH_LOTTO_NUMBER("로또 번호와 중복된 보너스 번호입니다.", ModifyingType.BONUS_NUMBER),
+    OUT_OF_RANGE_BONUS_NUMBER("범위에서 벗어난 수를 입력했습니다. 보너스 번호 범위는 1~45까지입니다.",
+            ModifyingType.BONUS_NUMBER),
+    NOT_UNIT("1000 단위의 금액을 입력해주세요", ModifyingType.MONEY_NUMBER)
+    ;
     ;
 
     private static final String PREFIX = "[ERROR] ";
--- src/main/java/lotto/exception/ModifyingType.java
+++ src/main/java/lotto/exception/ModifyingType.java
@@ -2,7 +2,9 @@ package lotto.exception;
 
 public enum ModifyingType {
     LOTTO_NUMBER(" 로또 번호을 다시 입력해주세요."),
-
+    MONEY_NUMBER(" 구입 금액을 다시 입력해주세요."),
+    EVERY_NUMBER(" 다시 입력해주세요."),
+    BONUS_NUMBER(" 보너스 번호를 다시 입력해주세요.")
     ;
 
     ModifyingType(String errorType) {
